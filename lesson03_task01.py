c990af61a0f8328c0371a37fcaf394bd24a2b986
k.kozhevnikov <dev@kozhevnikovspb.ru>
2024-10-04 15:01:13 +0300

    "lesson03 task01 - done"

---
+++ lesson03_task01.py
@@ -0,0 +1,7 @@
+print("Введите вид животного: ", end='')
+type_animal = input()
+print("Укажите возраст животного: ", end='')
+age_animal = input()
+print("Введите кличку животного: ", end='')
+name_animal = input()
+print("На прием записан", type_animal, "по кличке", name_animal + ".", "Возраст:", age_animal)
